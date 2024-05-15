924c787cb99cfcaf267883aae4929b02b57792c3
ranawareswapnil <121781206+ranawareswapnil@users.noreply.github.com>
2024-05-15 18:48:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-project-terraform-batch-28"
-    key = "main"
+    key = "praveee"
     region = "us-west-1"
     dynamodb_table = "my-dynamodb-table"
   }
