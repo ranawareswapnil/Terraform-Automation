5140dc8bca207bd2a26a2be75fa302402054cf04
ranawareswapnil <121781206+ranawareswapnil@users.noreply.github.com>
2024-05-15 16:58:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "us-west-1" 
 }
 
 variable "key_name" { 
